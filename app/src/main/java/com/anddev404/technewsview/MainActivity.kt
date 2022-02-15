7ca9e1f771503b0b60f4aa7bf3bf1ba77f5ca2fe
anddev404 <marcinek07@gmail.com>
2022-02-15 18:20:09 +0100

    test passing arguments to NewsListFragment

--- app/src/main/java/com/anddev404/technewsview/MainActivity.kt
+++ app/src/main/java/com/anddev404/technewsview/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.anddev404.technewsview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.anddev404.tech_news_views.NewsListFragment
+import com.anddev404.tech_news_views.placeholder.NewsItem
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newsFragment1: NewsListFragment =
+            NewsListFragment.newInstance(2, NewsItem.getExampleItemList())
+
+        getSupportFragmentManager().beginTransaction()
+            .add(R.id.fragmentNewsView, newsFragment1).commit();
+
     }
 }
