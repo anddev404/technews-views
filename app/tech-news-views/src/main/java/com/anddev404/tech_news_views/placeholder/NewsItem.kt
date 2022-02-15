a7702795b6cc15f1b692fbc912140c552961683e
anddev404 <marcinek07@gmail.com>
2022-02-15 21:09:49 +0100

    Update NewsItem.kt

--- app/tech-news-views/src/main/java/com/anddev404/tech_news_views/placeholder/NewsItem.kt
+++ app/tech-news-views/src/main/java/com/anddev404/tech_news_views/placeholder/NewsItem.kt
@@ -15,7 +15,25 @@ class NewsItem(var header: String) : Parcelable {
         }
 
         fun getExampleItemList(): ArrayList<NewsItem> {
-            return arrayListOf(NewsItem("header 1"), NewsItem("header 2"), NewsItem("header 3"))
+            return arrayListOf(
+                NewsItem("header 1"),
+                NewsItem("header 2"),
+                NewsItem("header 3"),
+                NewsItem("header 4"),
+                NewsItem("header 5"),
+                NewsItem("header 6")
+            )
+        }
+
+        fun getExampleItemList2(): ArrayList<NewsItem> {
+            return arrayListOf(
+                NewsItem("1. header"),
+                NewsItem("2. header"),
+                NewsItem("3. header"),
+                NewsItem("4. header"),
+                NewsItem("5. header"),
+                NewsItem("6. header")
+            )
         }
     }
 
