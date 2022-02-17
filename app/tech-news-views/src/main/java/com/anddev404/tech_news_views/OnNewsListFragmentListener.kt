e17b4ddd352d90a6a3b637502e4d201f3300e670
anddev404 <marcinek07@gmail.com>
2022-02-17 22:05:48 +0100

    Update OnNewsListFragmentListener.kt

--- app/tech-news-views/src/main/java/com/anddev404/tech_news_views/OnNewsListFragmentListener.kt
+++ app/tech-news-views/src/main/java/com/anddev404/tech_news_views/OnNewsListFragmentListener.kt
@@ -1,6 +1,9 @@
 package com.anddev404.tech_news_views
 
+import com.anddev404.tech_news_views.placeholder.NewsItem
+
 interface OnNewsListFragmentListener {
 
     fun updateList(pageNumber: Int)
+    fun tapItem(itemPosition: Int, newsItem: NewsItem)
 }
