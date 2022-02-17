a5a5ba0d772eec1c08c1afc4be036afcbe423593
anddev404 <marcinek07@gmail.com>
2022-02-17 22:40:01 +0100

    Update OnNewsListFragmentListener.kt

--- app/tech-news-views/src/main/java/com/anddev404/tech_news_views/OnNewsListFragmentListener.kt
+++ app/tech-news-views/src/main/java/com/anddev404/tech_news_views/OnNewsListFragmentListener.kt
@@ -1,11 +1,12 @@
 package com.anddev404.tech_news_views
 
+import android.widget.ImageView
 import com.anddev404.tech_news_views.placeholder.NewsItem
 
 interface OnNewsListFragmentListener {
 
     fun updateList(pageNumber: Int)
     fun tapItem(itemPosition: Int, newsItem: NewsItem)
-    fun getImage(url: String, newsItem: NewsItem)
+    fun setImage(url: String, newsItem: NewsItem, imagewView: ImageView)
 
 }
