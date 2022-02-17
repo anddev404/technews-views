6ba5e3eb0eef212a0ae2563ef317aa7834e43647
anddev404 <marcinek07@gmail.com>
2022-02-17 22:37:15 +0100

    update model

--- app/tech-news-views/src/main/java/com/anddev404/tech_news_views/placeholder/NewsItem.kt
+++ app/tech-news-views/src/main/java/com/anddev404/tech_news_views/placeholder/NewsItem.kt
@@ -5,7 +5,7 @@ import kotlinx.android.parcel.Parcelize
 
 
 @Parcelize
-class NewsItem(var header: String) : Parcelable {
+class NewsItem(var header: String, var imageUrl: String) : Parcelable {
 
 
     companion object {
@@ -18,19 +18,42 @@ class NewsItem(var header: String) : Parcelable {
 
             val exampleListOfItems = ArrayList<NewsItem>()
             for (i in 0..100) {
-                exampleListOfItems.add(NewsItem("header $i"))
+                exampleListOfItems.add(
+                    NewsItem(
+                        "header $i",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR563zwATHZ8pvggtUl0sQc5OgQxFL3GoJcLg&usqp=CAU"
+                    )
+                )
             }
             return exampleListOfItems
         }
 
         fun getShortExampleItemList(): ArrayList<NewsItem> {
             return arrayListOf(
-                NewsItem("1. header"),
-                NewsItem("2. header"),
-                NewsItem("3. header"),
-                NewsItem("4. header"),
-                NewsItem("5. header"),
-                NewsItem("6. header")
+                NewsItem(
+                    "1. header",
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR563zwATHZ8pvggtUl0sQc5OgQxFL3GoJcLg&usqp=CAU"
+                ),
+                NewsItem(
+                    "2. header",
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDu726oeFoAfkD5EpO-s2rzc8y1Wo_TClKFA&usqp=CAU"
+                ),
+                NewsItem(
+                    "3. header",
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR563zwATHZ8pvggtUl0sQc5OgQxFL3GoJcLg&usqp=CAU"
+                ),
+                NewsItem(
+                    "4. header",
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDu726oeFoAfkD5EpO-s2rzc8y1Wo_TClKFA&usqp=CAU"
+                ),
+                NewsItem(
+                    "5. header",
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR563zwATHZ8pvggtUl0sQc5OgQxFL3GoJcLg&usqp=CAU"
+                ),
+                NewsItem(
+                    "6. header",
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDu726oeFoAfkD5EpO-s2rzc8y1Wo_TClKFA&usqp=CAU"
+                )
             )
         }
     }
