2e7a1b9d90d696af33f012c6dfff6f5c309872ea
anddev404 <marcinek07@gmail.com>
2022-03-09 17:37:27 +0100

    remove arg pageNumber in OnNewsListFragmentListener

--- app/tech-news-views/src/main/java/com/anddev404/tech_news_views/OnNewsListFragmentListener.kt
+++ app/tech-news-views/src/main/java/com/anddev404/tech_news_views/OnNewsListFragmentListener.kt
@@ -5,7 +5,7 @@ import com.anddev404.tech_news_views.placeholder.NewsItem
 
 interface OnNewsListFragmentListener {
 
-    fun updateList(pageNumber: Int)
+    fun updateList()
     fun tapItem(itemPosition: Int, newsItem: NewsItem)
     fun setImage(url: String, newsItem: NewsItem, imageView: ImageView)
 
