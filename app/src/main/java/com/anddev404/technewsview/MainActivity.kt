0c56b04954acd4c83c83b9cdf09cdf1f59b153a5
anddev404 <marcinek07@gmail.com>
2022-04-20 13:58:08 +0200

    test NewsDetailsFragment

--- app/src/main/java/com/anddev404/technewsview/MainActivity.kt
+++ app/src/main/java/com/anddev404/technewsview/MainActivity.kt
@@ -1,70 +1,32 @@
 package com.anddev404.technewsview
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.anddev404.tech_news_views.NewsListFragment
-import com.anddev404.tech_news_views.OnNewsListFragmentListener
-import com.anddev404.tech_news_views.placeholder.NewsItem
-import com.squareup.picasso.Picasso
+import com.anddev404.tech_news_views.showNewsDetailsFragment.NewsDetailsFragment
 
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    lateinit var details: NewsDetailsFragment
+
+    override
+
+    fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val newsFragment1: NewsListFragment =
-            NewsListFragment.newInstance(1, NewsItem.getShortExampleItemList())
+
+        details =
+            NewsDetailsFragment.newInstance("https://www.gsmmaniak.pl/1302493/android-13-telefony-xiaomi-bez-aktualizacji/")
 
         getSupportFragmentManager().beginTransaction()
-            .replace(R.id.fragmentNewsView, newsFragment1).commit();
-
-        newsFragment1.setOnNewsListFragmentListener(object : OnNewsListFragmentListener {
-            override fun updateList(pageNumber: Int) {
-                Log.d("MARCIN", "download page: $pageNumber");
-                //newsFragment1.addItems(NewsItem.getShortExampleItemList())
-            }
-
-            override fun tapItem(itemPosition: Int, newsItem: NewsItem) {
-                Log.d("MARCIN", "tap: $itemPosition");
-
-            }
-
-            override fun setImage(url: String, newsItem: NewsItem, imageView: ImageView) {
-                Picasso.with(applicationContext)
-                    .load(url)
-                    .placeholder(
-                        applicationContext.getResources()
-                            .getDrawable(R.drawable.ic_launcher_foreground)
-                    )
-                    .error(
-                        applicationContext.getResources()
-                            .getDrawable(R.drawable.ic_launcher_background)
-                    )
-                    .into(imageView)
-            }
-
-
-        })
-
-        val newsFragment2 = supportFragmentManager.findFragmentById(R.id.staticFragmentNewsView)
-        newsFragment2?.let {
-            if (newsFragment2 is NewsListFragment) {
-                with(newsFragment2) {
-                    // setData(4, NewsItem.getShortExampleItemList())
-                }
-            }
-        }
-
-        var button = findViewById<Button>(R.id.test_button)
-        button.setOnClickListener({
-            if (newsFragment1 is NewsListFragment) {
-                newsFragment1.addItems(NewsItem.getShortExampleItemList())
-            }
-        })
+            .add(R.id.fragmentNewsView, details).commit();
 
     }
+
+    override fun onBackPressed() {
+        details.goBack()
+    }
+    //  super.onBackPressed()
+
 }
