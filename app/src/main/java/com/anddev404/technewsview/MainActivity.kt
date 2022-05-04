0e29de82a2e437d91cb5df9789157ec982e9d600
anddev404 <marcinek07@gmail.com>
2022-05-04 22:33:56 +0200

    manual view tests

--- app/src/main/java/com/anddev404/technewsview/MainActivity.kt
+++ app/src/main/java/com/anddev404/technewsview/MainActivity.kt
@@ -2,31 +2,31 @@ package com.anddev404.technewsview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.anddev404.tech_news_views.newsListFragment.NewsListFragment
+import com.anddev404.tech_news_views.newsListFragment.model.NewsItem
 import com.anddev404.tech_news_views.showNewsDetailsFragment.NewsDetailsFragment
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var list: NewsListFragment
     lateinit var details: NewsDetailsFragment
 
     override
-
     fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
+        list =
+            NewsListFragment.newInstance(1, NewsItem.getShortExampleItemList().toTypedArray())
+        getSupportFragmentManager().beginTransaction()
+            .add(R.id.fragmentNewsView, list).commit();
 
         details =
             NewsDetailsFragment.newInstance("https://www.gsmmaniak.pl/1302493/android-13-telefony-xiaomi-bez-aktualizacji/")
-
         getSupportFragmentManager().beginTransaction()
-            .add(R.id.fragmentNewsView, details).commit();
-
+            .add(R.id.fragmentDetails, details).commit();
     }
-
-    override fun onBackPressed() {
-        details.goBack()
-    }
-    //  super.onBackPressed()
 
 }
